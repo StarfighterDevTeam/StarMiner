466acac3e084ee124f47a2576ae95ce2a5d8b460
Wister <bobwister@gmail.com>
2026-05-16 20:03:23 +0200

    Game icon

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ from game import Game
 def main():
     pygame.init()
     pygame.display.set_caption("StarMiner")
+    try:
+        icon = pygame.image.load("assets/2D/icon.png")
+        pygame.display.set_icon(icon)
+    except Exception:
+        pass
     screen = pygame.display.set_mode((SCREEN_W, SCREEN_H))
     game = Game(screen)
     game.run()
